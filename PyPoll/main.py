4a584552cf3bdfec9d5eb760e1c085fbbb6bc8f8
carchu9 <122560104+carchu9@users.noreply.github.com>
2023-03-13 12:54:40 -0600

    Updating

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -35,93 +35,79 @@ In addition, your final script should both print the analysis to the terminal an
 # Import Dependencies
 import os
 import csv
-import pandas as pd
 
-# Creating a path to the csv file 
-csv_path = ("C:/Users/17202/Documents/GitHub/Python-Challenge/PyPoll/Resources/election_data.csv")
-
-# Using pandas to create a dataframe and read the csv
-election_df = pd.read_csv(csv_path)
-
-# This is the path for my txt file that will be used at the end
+# Creating a path to the csv and txt file 
+csv_path = os.path.join("C:/Users/17202/Documents/GitHub/Python-Challenge/PyPoll/Resources/election_data.csv")
 output = ("C:/Users/17202/Documents/GitHub/Python-Challenge/PyPoll/Analysis/Election Results.txt")
 
+# Creating variables
+TotalVotes = 0
+Candidates = []
+candidatevotes = {}
 
-# Viewing the head of the dataframe
-election_df.head()
+# Open csv file
+with open (csv_path) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader)
+    firstrow = next(csvreader)
 
-# Counting the length of the ballot ID's to get a number of votes placed
-totalvotes = len(election_df["Ballot ID"].value_counts())
-
-# Looking up all the unique names of the candidates
-candidates = election_df["Candidate"].unique()
-
-# First gathering how many times the candidates  were mentioned in the data from
-# the Candidate column. Then counting the length of the  data to get the amount
-# of votes for each candidate
-DDraw = election_df.loc[(election_df["Candidate"] == "Diana DeGette")]
-DD= len(DDraw.value_counts())
-
-RADraw = election_df.loc[(election_df["Candidate"] == "Raymon Anthony Doane")]
-RAD = len(RADraw.value_counts())
-
-CCSraw = election_df.loc[(election_df["Candidate"] == "Charles Casper Stockham")]
-CCS = len(CCSraw.value_counts())
-
-# Dividing each candidate's votes by the total amount of votes to get the 
-# percentage of votes per candidate
-
-CCSper = (CCS/totalvotes) * 100
-
-DDper = (DD/totalvotes) * 100
-
-RADper = (RAD/totalvotes) * 100
-
-# Counting the Candidate column to see who's name appears the most to show who 
-# won the election
-winner = (election_df["Candidate"].value_counts().idxmax())
-
+    # Counting total votes
+    TotalVotes += 1
+    Candidates = firstrow[2]
+    
+    # Reading each row of data
+    for row in csvreader:
+        
+        TotalVotes += 1
+        Candidates = row[2]
+        
+        if row[2] not in candidatevotes:
+            candidatevotes[row[2]] = 1
+        
+        else:
+            candidatevotes[row[2]] += 1
 
 # Printing the results in terminal
 print("Election Results")
 
 print("----------------------------------")
 
-print("Total Votes: " + str(totalvotes))
+print("Total Votes: " + str(TotalVotes))
 
 print("----------------------------------")
 
-print("Charles Casper Stockham: " + str(round(CCSper,3)) + "%" + " (" + str(CCS) +  ")" )
-
-print("Diane DeGette: " + str(round(DDper,3)) + "%" + " (" + str(DD) +  ")" )
-
-print("Raymon Anthony Doane: " + str(round(RADper,3)) + "%" + " (" + str(RAD) +  ")" )
+for candidate, votes in candidatevotes.items():
+    print(candidate + ": " + "{:.3%}".format(votes/TotalVotes) + "  (" + str(votes) + ")")
 
 print("----------------------------------")
 
-print("Winner: " + winner)
+winner = max(candidatevotes, key=candidatevotes.get)
+    
+print(f"Winner: {winner}")
 
 print("----------------------------------")
 
 # Write this to output file
 
 with open(output, "w") as f:
+    
     f.write("Election Results\n")
     f.write("\n")
-    f.write("----------------------------------\n")
+    f.write("---------------------------\n")
     f.write("\n")
-    f.write("Total Votes: " + str(totalvotes) +"\n")
+    f.write("Total Votes: " + str(TotalVotes)+ "\n")
     f.write("\n")
-    f.write("----------------------------------\n")
+    f.write("---------------------------\n")
     f.write("\n")
-    f.write("Charles Casper Stockham: " + str(round(CCSper,3)) + "%" + " (" + str(CCS) +  ")\n")
-    f.write("Diane DeGette: " + str(round(DDper,3)) + "%" + " (" + str(DD) +  ")\n" )
-    f.write("Raymon Anthony Doane: " + str(round(RADper,3)) + "%" + " (" + str(RAD) +  ")\n" )
+    
+    for candidate, votes in candidatevotes.items():
+        f.write(candidate + ": " + "{:.3%}".format(votes/TotalVotes) + "  (" + str(votes) + ") +\n")
     f.write("\n")
-    f.write("----------------------------------\n")
+    f.write("---------------------------\n")
     f.write("\n")
-    f.write("Winner: " + winner +"\n")
+    
+    winner = max(candidatevotes, key=candidatevotes.get)
+    f.write(f"Winner: {winner}+ \n")
     f.write("\n")
-    f.write("----------------------------------\n")
 
 
